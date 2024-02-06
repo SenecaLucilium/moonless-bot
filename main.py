7ec48b07c52c807478b904601d3fc9dc32d68896
SenecaLucilium <SenecaLuciliumCryptoGuard@protonmail.com>
2024-02-06 10:36:27 +0300

    test deleted

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-from BackEnd.telegraph import createTelegraph
+from FrontEnd.botAPI import BotAPI
 
-
-response = createTelegraph ('test1')
-print (response['url'])
-import markdown
-
-with open ('C:\Main\Documents\Obsidian Vault\Moonless Project\BotTests' + '/test1.md', 'r', encoding='utf-8') as file:
-    text = file.read ()
-
-    title = text.partition ('\n')[0][2:]
-    print (title.encode ('utf-8'))
-
-    html = markdown.markdown (text)
+BotAPI()
